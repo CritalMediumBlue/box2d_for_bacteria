b1128006682bffaf15d586982203bf2eb64fb3bf
CritalMediumBlue <santandergualdron@gmail.com>
2023-11-12 14:49:54 +0100

    added custom colors

--- build/dynamics/b2_world.js
+++ build/dynamics/b2_world.js
@@ -521,11 +521,11 @@ System.register(["../common/b2_settings.js", "../common/b2_math.js", "../common/
                                     this.DrawShape(f, new b2_draw_js_1.b2Color(1.0, 0.0, 0.0));
                                 }
                                 else if (!b.IsEnabled()) {
-                                    color.SetRGB(0.5, 0.5, 0.3);//
+                                    color.SetRGB(0.5, 0.5, 0.3);
                                     this.DrawShape(f, color);
                                 }
                                 else if (b.GetType() === b2_body_js_1.b2BodyType.b2_staticBody) {
-                                    color.SetRGB(0.5, 0.9, 0.5);// green
+                                    color.SetRGB(0.5, 0.9, 0.5);
                                     this.DrawShape(f, color);
                                 }
                                 else if (b.GetType() === b2_body_js_1.b2BodyType.b2_kinematicBody) {
@@ -535,12 +535,12 @@ System.register(["../common/b2_settings.js", "../common/b2_math.js", "../common/
                                 else if (!b.IsAwake()) {
                                     color.SetRGB(0.6, 0.6, 0.6);
                                     this.DrawShape(f, color);
-                                }
-                                else if (b.myCustomColor) {
+                                } else if (b.myCustomColor) {
+                                    color.SetRGB(0.6, 0.6, 0.6);
                                     this.DrawShape(f, b.myCustomColor);
                                 }
                                 else {
-                                    color.SetRGB(0.9, 0.1, 0.7);// red
+                                    color.SetRGB(0.9, 0.7, 0.7);
                                     this.DrawShape(f, color);
                                 }
                             }
